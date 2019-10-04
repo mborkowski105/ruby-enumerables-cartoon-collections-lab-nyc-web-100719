7aa2e68d3362271c5b77d1328dfc58b8504abb09
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-04 16:18:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,8 @@ def long_planeteer_calls(array)
   array.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  
 end
