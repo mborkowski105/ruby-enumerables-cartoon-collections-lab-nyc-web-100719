ce477fd2bd5f1922bf95480b988bad749ccaab7c
Matthew Borkowski <barnabytheplumber@gmail.com>
2019-10-04 16:13:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,8 @@ def roll_call_dwarves(array)
 end
 
 def summon_captain_planet(array)
-  array.map{ |x| x.capitalize() }
-  array.map{ |x| x.push("!") }
+  array.map{ |x| x << capitalize() }
+  array.map{ |x| x << push("!") }
 end
 
 def long_planeteer_calls# code an argument here
